0d7e460921ec8df335b4633a03289efb21c1cd70
William B <blanchettwa@g.cofc.edu>
2017-02-03 15:59:37 -0500

    Created rudimentary working app

--- app/src/main/java/tk/wablanchett/inclass_activity_01_blanchett/MainActivity.java
+++ app/src/main/java/tk/wablanchett/inclass_activity_01_blanchett/MainActivity.java
@@ -2,7 +2,9 @@ package tk.wablanchett.inclass_activity_01_blanchett;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.drawable.DrawerArrowDrawable;
 import android.widget.DatePicker;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
@@ -11,8 +13,8 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     DatePicker daDate;
-    TimePicker daTime = (TimePicker) findViewById(R.id.datime);
-
+    TimePicker daTime;
+    TextView dateWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,20 +22,22 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        daDate = (DatePicker) findViewById(R.id.dadate);
+        daTime = (TimePicker) findViewById(R.id.datime);
+        dateWindow = (TextView) findViewById(R.id.dateWindow);
+        dateWindow.setText(Calendar.getInstance().getTime().toString());
 
-        daDate.init(2017, 2, 3, new DatePicker.OnDateChangedListener()
-        {
-            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+
+        dateWindow.setText(Calendar.getInstance().getTime().toString());
+            daDate.init(2017, 2, 3, new DatePicker.OnDateChangedListener()
             {
-                Calendar cal = Calendar.getInstance();
-                cal.set(year,monthOfYear,dayOfMonth);
-
-
-            }
-
-
-
-        });
+                public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+                {
+                    Calendar cal = Calendar.getInstance();
+                    cal.set(year,monthOfYear,dayOfMonth);
+                    dateWindow.setText(cal.getTime().toString());
+                }
+            });
 
 
 
