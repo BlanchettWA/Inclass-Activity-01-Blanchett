85e314543e9856e6cab684a127d90a450669541a
William B <blanchettwa@g.cofc.edu>
2017-02-03 13:47:46 -0500

    Began creating the pickers in the view.

--- app/src/main/java/tk/wablanchett/inclass_activity_01_blanchett/MainActivity.java
+++ app/src/main/java/tk/wablanchett/inclass_activity_01_blanchett/MainActivity.java
@@ -2,12 +2,47 @@ package tk.wablanchett.inclass_activity_01_blanchett;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatePicker daDate;
+    TimePicker daTime = (TimePicker) findViewById(R.id.datime);
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        daDate.init(2017, 2, 3, new DatePicker.OnDateChangedListener()
+        {
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+            {
+                Calendar cal = Calendar.getInstance();
+                cal.set(year,monthOfYear,dayOfMonth);
+
+
+            }
+
+
+
+        });
+
+
+
+
+
+
+
     }
 }
+
+
+
